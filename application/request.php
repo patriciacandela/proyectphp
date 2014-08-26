b96290ed9f4aaf16c0dfa9a2bd9a266dcf66c2d1
ADSI03 <ADSI03@10.100.113.78>
2014-08-26 14:22:19 -0500

    primer archivo mvc

--- application/request.php
+++ application/request.php
@@ -1,8 +1,40 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    class Request {
+        private $_controlador;
+        private $_metodo;
+        private $_argumentos;
+        
+        public function __construct (){
+            if (isset($_GET ['url'])){
+                $url= filter_input(INPUT_GET, 'url',FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                $url =  array_filter($url);
+                $con =  strtolower(array_shift($url));
+                $con =  explode('.php', $con);
+            }
+            $this->_controlador = array_shift($con);
+            $this->_metodo = array_shift($url);
+            $this->_argumentos = $url;
+            
+            if (!$this->_controlador){
+                $this->_controlador=  DEFAULT_CONTROLLER;
+            }
+            if (!$this->_metodo){
+                $this->_metodo='index';
+            }
+            if (!isset($this->_argumentos)){
+                $this->_argumentos=  array ();
+            }
+    }
+    
+            public function getControlador (){
+             return $this->_controlador;
+             }
+            public function getMetodo(){
+             return $this->_metodo;
+             }
+             public function getArgs (){
+             return $this->_argumentos;
+             }
+   }
+?>
--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
    define('DS', DIRECTORY_SEPARATOR);
    define('ROOT',  realpath(dirname(__FILE__)).DS );
+   define('APP_PATH', ROOT. 'application'.DS);
    
-   echo ROOT;
+   require_once APP_PATH.'config.php';
+   require_once APP_PATH.'request.php';
+   require_once APP_PATH.'bootstrap.php';
+   require_once APP_PATH.'controller.php';
+   require_once APP_PATH.'model.php';
+   require_once APP_PATH.'view.php';
+   require_once APP_PATH.'registro.php';
+   
+  $r =new Request();
+  echo $r->getControlador().'<br>';
+  echo $r->getMetodo().'<pre>';
+  print_r($r->getArgs());
+ 
 ?>
 
